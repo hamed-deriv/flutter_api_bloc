ffc7b6b9c956f13161e39e47ba88af2ffa5409a2
hamed-fs <hamed@firstsource.io>
2022-02-20 09:01:14 +0330

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Text('Flutter Demo Home Page'),
+    );
+  }
+}
